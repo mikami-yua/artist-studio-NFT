fd8306e2a0d340edbec3ad7828cdab08f856e3e9
mikami-yua <lovejiaxi_1@163.com>
2025-02-09 23:17:03 +0800

    ether包版本问题，版本高了页面不可用，版本低了UT过不了

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.28;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
+// 合约的部署者在部署的时候存入一笔钱，过了锁定期可以提取这笔钱
 contract Lock {
     uint public unlockTime;
     address payable public owner;
@@ -18,6 +19,7 @@ contract Lock {
         );
 
         unlockTime = _unlockTime;
+        // owner设置为payable的地址，可以通过transfer和send进行转账
         owner = payable(msg.sender);
     }
 
@@ -26,10 +28,12 @@ contract Lock {
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        // 谁存入合约谁可以提款
         require(msg.sender == owner, "You aren't the owner");
 
         emit Withdrawal(address(this).balance, block.timestamp);
 
+        // 提款逻辑：调用owner的transfer方法（向owner转账），把当前合约address(this)的余额转账给owner
         owner.transfer(address(this).balance);
     }
 }
